000fbad42322f8b4bfcfcf1f84cf46b6930f5428
Matt Cordell <matt@evolution7.com.au>
2015-09-28 21:06:36 +0200

    added modal component, tweaked header js

--- javascript/_header.js
+++ javascript/_header.js
@@ -2,7 +2,13 @@ var $ = require('jquery');
 
 var $body = $('body');
 var $toggle = $('.js-mobile-nav');
+var $overlay = $('.header__overlay');
 
 $toggle.on('click', function(){
     $body.toggleClass('mobile-nav-active');
+    $overlay.on('click', function(){
+        $body.removeClass('mobile-nav-active');
+        $(this).off('click');
+    });
 });
+
